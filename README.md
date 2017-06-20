13608517d6b260afc9f8d5f90c39322e4158377b
JP Erasmus <jperasmus11@gmail.com>
2017-06-20 09:52:07 +0200

    Test files

---
+++ README.md
@@ -0,0 +1,26 @@
+## What should I do?
+
+To use the tests, you will need to install [Node](https://nodejs.org/).
+
+From the root directory of this repo, run:
+
+    npm install (or yarn)
+    npm start
+
+You can then view the tests in your browser at
+[http://localhost:4444](http://localhost:4444).
+
+When you visit that page, all of the tests should be failing; your job is to
+get the tests to pass. To do this, you'll need to refer to the tests in the
+files in the `tests/app` directory, and edit the files in the `app/` directory.
+Once you update a test, you can reload the test page in the browser to see
+whether it worked.
+
+Please see how many of the tests you can make pass, but it is not necessary for
+all to pass, it is simply to assess what you already know and what you still
+need to learn.
+
+### Available dependencies
+
+The repo includes jQuery, Backbone, and Underscore. You can use these
+libraries when writing your solutions!
---
+++ tests/app/recursion.js
@@ -0,0 +1,51 @@
+if ( typeof window === 'undefined' ) {
+  require('../../app/recursion');
+  var expect = require('chai').expect;
+  var _ = require('underscore');
+}
+
+describe('recursion', function() {
+  var fileData = {
+    dir: 'app',
+    files: [
+      'index.html',
+      {
+        dir: 'js',
+        files: [
+          'main.js',
+          'app.js',
+          'misc.js',
+          {
+            dir: 'vendor',
+            files: [
+              'jquery.js',
+              'underscore.js'
+            ]
+          }
+        ]
+      },
+      {
+        dir: 'css',
+        files: [
+          'reset.css',
+          'main.css'
+        ]
+      }
+    ]
+  };
+
+  it('you should be able to return a list of files from the data', function() {
+    var result = recursionAnswers.listFiles(fileData);
+    expect(result.length).to.eql(8);
+    expect(result.indexOf('index.html') > -1).to.be.ok;
+    expect(result.indexOf('main.js') > -1).to.be.ok;
+    expect(result.indexOf('underscore.js') > -1).to.be.ok;
+  });
+
+  it('you should be able to return a list of files in a subdir', function() {
+    var result = recursionAnswers.listFiles(fileData, 'js');
+    expect(result.length).to.eql(5);
+    expect(result.indexOf('main.js') > -1).to.be.ok;
+    expect(result.indexOf('underscore.js') > -1).to.be.ok;
+  });
+});
